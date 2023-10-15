768747abc1b9ed00fad7a8221b96da3e50f6029b
subham007-coder <dsubham109@gmail.com>
2023-10-15 13:42:20 +0530

    js modify

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
-const pvrBtn = document.querySelector(".carousel-control-prev")
+const pvrBtn = document.querySelector(".carousel-control-prev");
 
-const nextBtn = document.querySelector(".carousel-control-next")
+const nextBtn = document.querySelector(".carousel-control-next");
 
 let flag = 0;
 
@@ -12,7 +12,7 @@ pvrBtn.addEventListener("click", () => {
 
     if (flag == 6) {
         flag = 0;
-        container.style.backgroundColor = "#242424"
+        container.style.backgroundColor = "#242424";
         console.log(flag);
     }
 
@@ -20,19 +20,19 @@ pvrBtn.addEventListener("click", () => {
         container.style.backgroundColor = "#37161D"
     }
     if (flag == 2) {
-        container.style.backgroundColor = "#4F5855"
+        container.style.backgroundColor = "#4F5855";
     }
     if (flag == 3) {
-        container.style.backgroundColor = "#C4682C"
+        container.style.backgroundColor = "#C4682C";
     }
     if (flag == 4) {
-        container.style.backgroundColor = "#28334D"
+        container.style.backgroundColor = "#28334D";
     }
     if (flag == 5) {
-        container.style.backgroundColor = "#243133"
+        container.style.backgroundColor = "#243133";
     }
     if (flag == 6 || flag == 0) {
-        container.style.backgroundColor = "rgb(36, 36, 36)"
+        container.style.backgroundColor = "rgb(36, 36, 36)";
     }
 })
 
@@ -44,27 +44,27 @@ nextBtn.addEventListener("click", () => {
 
     if (flagnext == 6) {
         flagnext = 0;
-        container.style.backgroundColor = "#242424"
+        container.style.backgroundColor = "#242424";
         console.log(flagnext);
     }
 
     if (flagnext == 1) {
-        container.style.backgroundColor = "#243133"
+        container.style.backgroundColor = "#243133";
     }
     if (flagnext == 2) {
-        container.style.backgroundColor = "#28334D"
+        container.style.backgroundColor = "#28334D";
     }
     if (flagnext == 3) {
-        container.style.backgroundColor = "#C4682C"
+        container.style.backgroundColor = "#C4682C";
     }
     if (flagnext == 4) {
-        container.style.backgroundColor = "#4F5855"
+        container.style.backgroundColor = "#4F5855";
     }
     if (flagnext == 5) {
-        container.style.backgroundColor = "#37161D"
+        container.style.backgroundColor = "#37161D";
     }
     if (flagnext == 6 || flagnext == 0) {
-        container.style.backgroundColor = "rgb(36, 36, 36)"
+        container.style.backgroundColor = "rgb(36, 36, 36)";
         console.log("subham");
     }
 })
