cfd7d0d2dc38db3551554551643547853c5631f9
subham007-coder <dsubham109@gmail.com>
2023-10-15 13:33:33 +0530

    js

--- app.js
+++ app.js
@@ -16,28 +16,27 @@ pvrBtn.addEventListener("click", () => {
         console.log(flag);
     }
 
-        if (flag == 1) {
-            container.style.backgroundColor = "#37161D"
-        }
-        if (flag == 2) {
-            container.style.backgroundColor = "#4F5855"
-        }
-        if (flag == 3) {
-            container.style.backgroundColor = "#C4682C"
-        }
-        if (flag == 4) {
-            container.style.backgroundColor = "#28334D"
-        }
-        if (flag == 5) {
-            container.style.backgroundColor = "#243133"
-        }
-        if (flag == 6 || flag == 0) {
-            container.style.backgroundColor = "rgb(36, 36, 36)"
-            console.log("subham");
-        }
+    if (flag == 1) {
+        container.style.backgroundColor = "#37161D"
+    }
+    if (flag == 2) {
+        container.style.backgroundColor = "#4F5855"
+    }
+    if (flag == 3) {
+        container.style.backgroundColor = "#C4682C"
+    }
+    if (flag == 4) {
+        container.style.backgroundColor = "#28334D"
+    }
+    if (flag == 5) {
+        container.style.backgroundColor = "#243133"
+    }
+    if (flag == 6 || flag == 0) {
+        container.style.backgroundColor = "rgb(36, 36, 36)"
+    }
 })
 
-    let flagnext = 0;
+let flagnext = 0;
 
 nextBtn.addEventListener("click", () => {
     flagnext += 1;
@@ -49,23 +48,23 @@ nextBtn.addEventListener("click", () => {
         console.log(flagnext);
     }
 
-        if (flagnext == 1) {
-            container.style.backgroundColor = "#243133"
-        }
-        if (flagnext == 2) {
-            container.style.backgroundColor = "#28334D"
-        }
-        if (flagnext == 3) {
-            container.style.backgroundColor = "#C4682C"
-        }
-        if (flagnext == 4) {
-            container.style.backgroundColor = "#4F5855"
-        }
-        if (flagnext == 5) {
-            container.style.backgroundColor = "#37161D"
-        }
-        if (flagnext == 6 || flagnext == 0) {
-            container.style.backgroundColor = "rgb(36, 36, 36)"
-            console.log("subham");
-        }
+    if (flagnext == 1) {
+        container.style.backgroundColor = "#243133"
+    }
+    if (flagnext == 2) {
+        container.style.backgroundColor = "#28334D"
+    }
+    if (flagnext == 3) {
+        container.style.backgroundColor = "#C4682C"
+    }
+    if (flagnext == 4) {
+        container.style.backgroundColor = "#4F5855"
+    }
+    if (flagnext == 5) {
+        container.style.backgroundColor = "#37161D"
+    }
+    if (flagnext == 6 || flagnext == 0) {
+        container.style.backgroundColor = "rgb(36, 36, 36)"
+        console.log("subham");
+    }
 })
